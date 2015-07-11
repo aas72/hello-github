873b6d9b68490d472844b3917b0c23dc24e7e354
Arnout Saedt <asaedt@lennoc.com>
2015-07-11 11:57:24 +0200

    watch task created

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
 var gulp = require('gulp');
+var del = require('del');
 
-gulp.task('scripts', function() {
-  gulp.src('src/**/*.js')
-   .pipe(gulp.dest('dist'));
+var paths = {
+  scripts: 'src/*.js',
+  output: 'dist'
+};
+
+gulp.task('clean', function(cb) {
+  del([paths.output], cb);
 });
+
+gulp.task('scripts', ['clean'], function() {
+  gulp.src(paths.scripts)
+   .pipe(gulp.dest(paths.output));
+});
+
+gulp.task('watch', function() {
+  gulp.watch([paths.scripts], ['scripts']);
+});
+
+gulp.task('build', ['scripts']);
+gulp.task('serve', ['watch']);
