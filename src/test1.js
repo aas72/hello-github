a1f2fa37f25c935083d157838daf9c1ae83ea6dd
Arnout Saedt <asaedt@lennoc.com>
2015-07-12 11:11:49 +0200

    update

--- src/test1.js
+++ src/test1.js
@@ -3,8 +3,16 @@ angular
 
 angular
 	.module('helloGithub', ['templates'])
+	.controller('helloGithubController', ['$scope', function($scope) {
+		$scope.clickCount = 0;
+
+		$scope.clicked = function() {
+			$scope.clickCount++;
+		}
+	}])
 	.directive('helloGithub', function() {
 		return {
+			scope: {},
 			templateUrl: 'test1.html'
 		};
 	});
